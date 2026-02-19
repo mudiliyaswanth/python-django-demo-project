e582c04b5ef946aff4ab29520e1b36167d8a05ca
Yaswanth <147583250+mudiliyaswanth@users.noreply.github.com>
2026-02-19 23:37:09 +0530

    moved filename comment from line 3 to line 1

--- healthcare/models.py
+++ healthcare/models.py
@@ -1,7 +1,8 @@
+# healthcare/models.py
+
 from django.db import models
 
 # Create your models here.
-# healthcare/models.py
 
 from healthcare.patients.models import Patient
 from healthcare.vitals.models import VitalSign
